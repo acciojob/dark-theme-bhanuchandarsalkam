371a4de25b460b689872e8f747662f691a570870
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-07-04 11:54:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,4 +2,8 @@
 function swapTheme(){
 	const element=document.getElementById("app");
 	element.style.color="black";
+	element.className = "night";
+	const btn = document.getElementById("swap");
+	btn.className = "button_night"
+	
 }
